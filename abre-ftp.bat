a6868843b7b8e4947ade051348a03f9764f01f37
Rodrigo Galvão Patriota <31571350+gabriel12345678901@users.noreply.github.com>
2019-05-22 11:49:46 -0300

    Baixa arquivos do FTP

---
+++ abre-ftp.bat
@@ -0,0 +1,22 @@
+@ftp -i -s:"%~f0"
+
+open plutao.telemar
+serede
+telemar@31
+cd decoders
+
+lcd C:\Users\tr642633\Documents\rodrigo\dms\ftp
+get DM0037_Miscelaneas_Aplicados_Click_2019_05_21.xlsx
+get DM0039_Antenas_Aplicadas_Baixa_2019_05_21.csv
+get DM0039_Decoders_Aplicados_Baixa_2019_05_21.csv
+get DM0039_LNBF_Aplicados_Baixa_2019_05_21.csv
+get DM0040_Materiais_Aplicados_Click_DTH_2019_05_21.xlsx
+
+
+
+
+disconnect
+bye
+
+
+
